0923ea124101ecaaa697abe43dbc86f9c4670cbf
VISHAL GARG <148087458+vishal145o@users.noreply.github.com>
2025-08-03 22:00:03 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ dotenv.config();
 const app = express();
 const PORT = 3000;
 
-const model = process.env.GEMINI_MODEL || 'gemini-2.5-flash';
+const model = process.env.GEMINI_MODEL || 'gemini-2.5-flash-preview-native-audio-dialog';
 const apiKey = process.env.GEMINI_API_KEY;
 
 app.use(cors());
